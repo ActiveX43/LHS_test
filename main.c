8055ab4d54a264c74432203a103a4d64116e7e6a
ActiveX43 <yunks2000@naver.com>
2022-01-06 14:11:39 +0900

    bug fixed, compiled

--- main.c
+++ main.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 
-int main (unsigned int argc, char[]* argv) {
-  int a = 3;
-  if (argc == 0) return 0;
-  else {
-    for (int i = 0; i < argc; i++) {
+int main (unsigned int argc, char* argv[]) {
+    int a = 3;
+    if (argc == 0) return 0;
+    else {
+        printf("argc: %d\n", argc);
+        for (int i = 1; i < argc; i++) {
       
-      printf("%d:, %s\n", i + 1, argv[i]);
+            printf("%d:, %s\n", i, argv[i]);
+        }
+    }
 
-  }
-
-  return a;
-
+    return a;
 
 }
